b5ca21b97b297d4c393b4fbc9c04a1a2d5a71b49
Daniel Usani <daniel.e.usani@gmail.com>
2018-02-26 20:05:31 -0500

    React JSX Exercises

--- jsx-exercises/src/index.js
+++ jsx-exercises/src/index.js
@@ -2,26 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 
-// function MyThing() {
-//   return (
-//     <div className="book">
-//       <div className="title">
-//         The Title
-//       </div>
-//       <div className="author">
-//         The Author
-//       </div>
-//       <ul className="stats">
-//         <li className="rating">
-//           5 stars
-//         </li>
-//         <li className="isbn">
-//           12-345678-910
-//         </li>
-//       </ul>
-//     </div>
-//   );
-// }
+function MyThing() {
+  return (
+    <div className="book">
+      <div className="title">
+        The Title
+      </div>
+      <div className="author">
+        The Author
+      </div>
+      <ul className="stats">
+        <li className="rating">
+          5 stars
+        </li>
+        <li className="isbn">
+          12-345678-910
+        </li>
+      </ul>
+    </div>
+  );
+}
 
 function NewThing() {
   return (
@@ -36,15 +36,28 @@ function NewThing() {
   );
 }
 
-// function Greeting() {
-//   var username = 'root';
-//   var username = undefined;
-//   var username = null;
-//   var username = false;
-//
-//   return (
-//
-//   );
-// }
+function Greeting() {
+  var message;
+  // var username = 'root';
+  var username = undefined;
+  // var username = null;
+  // var username = false;
 
-ReactDOM.render(<NewThing />, document.getElementById('root'));
+  var loggedIn;
+    if(typeof username === 'string'){
+      message = <h1>Welcome, {username}</h1>
+    }else if (typeof username === 'undefined' || typeof username === 'null') {
+      message = <h1>Not logged in.</h1>
+    } else {
+      message = <h1>Please try again</h1>
+    }
+
+
+  return (
+    <div>
+      {message}
+    </div>
+  );
+}
+
+ReactDOM.render(<Greeting />, document.getElementById('root'));
